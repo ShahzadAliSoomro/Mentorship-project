208937093d5dbc0891b2574e36185a88b46b89b6
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2024-01-13 23:29:38 +0500

    navbar done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,11 +18,11 @@ export default function Navbar() {
           </div>
           <button
             onClick={handleOpenMenu}
-            className="lg:hidden flex pt-2 items-center"
+            className="md:hidden flex pt-2 items-center"
           >
             <FiMenu className="text-3xl" />
           </button>
-          <div className="hidden lg:flex gap-8 items-center justify-center">
+          <div className="hidden md:flex gap-8 items-center justify-center">
             <Link href="/" className="text-black text-xl font-semibold">
               Curriculum
             </Link>
@@ -35,24 +35,21 @@ export default function Navbar() {
             <Link href="/" className="text-black text-xl font-semibold">
               Contact
             </Link>
-            <div className="hidden lg:flex justify-center border border-[#B1AAC6] p-3 rounded-lg">
-              <Link href="/" className="text-[#764DEF]">
+            <div className="hidden lg:flex justify-center text-[#764DEF] border border-[#B1AAC6] p-3 rounded-lg">
+              <Link href="/" className="">
                 Sign In
               </Link>
             </div>
             <div className="hidden lg:flex justify-center p-3 bg-[#764DEF] text-white rounded-lg">
               <Link href="/">Book Free Trial Today</Link>
             </div>
-            {/* Hamburger menu for mobile */}
-            <div className="lg:hidden">
-              {/* Add your mobile menu toggle logic here */}
-            </div>
+           
           </div>
         </div>
       </div>
       {/* /// mobile screen navbar /// */}
       {isOpen && (
-        <div className="lg:hidden absolute top-16 right-0 w-full">
+        <div className="md:hidden absolute top-16 right-0 w-full">
           <div className="flex flex-col gap-8 p-4">
             <Link href="/" className="text-black text-xl font-semibold">
               Curriculum
